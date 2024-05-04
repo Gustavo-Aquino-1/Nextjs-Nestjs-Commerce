d5695dff41055fd4e925ab3004651938f50fee63
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2024-05-04 16:57:14 -0300

    feat: products component update

--- frontend/components/Products.tsx
+++ frontend/components/Products.tsx
@@ -53,12 +53,12 @@ async function Products({ title, productId, filters }: ProductsProps) {
         className={`flex justify-between m-auto ${
           data.length >= 3 ? 'max-w-[95%]' : 'max-w-[50%]'
         }  max-lg:max-w-none gap-10 scrollbar-thumb-rounded-full scrollbar-track-rounded-full max-md:pr-[50px] scrollbar-thin
-        lg:overflow-x-hidden max-lg:overflow-x-scroll overflow-y-hidden scrollbar-track-gray-200 scrollbar-thumb-gray-400 active:scrollbar-thumb-gray-500`}
+        lg:overflow-x-hidden max-lg:overflow-x-scroll overflow-y-hidden scrollbar-track-gray-200 scrollbar-thumb-gray-400 active:scrollbar-thumb-gray-500 pb-4`}
       >
         {data.map((e: any) => (
           <div
             key={e.id}
-            className='flex flex-col items-center justify-center max-md:items-start gap-2  lg:rounded-md lg:hover:scale-105 max-lg:bg-white max-lg:text-black'
+            className='flex flex-col items-center justify-center max-md:items-center gap-2  lg:hover:scale-105 max-lg:bg-white max-lg:text-black border-2 border-gray-200'
           >
             <Link className='z-0 scale-100' href={`/product/${e.id}`}>
               <Image
@@ -69,19 +69,21 @@ async function Products({ title, productId, filters }: ProductsProps) {
                 alt={e.name}
               />
             </Link>
-            <Link
-              href={`/product/${e.id}`}
-              className={`font-semibold hover:font-bold text-lg max-md:text-base`}
-            >
-              {e.name}
-            </Link>
-            <div className='pb-4'>
-              <p className='text-gray-500 line-through'>{`R$ ${
-                e.price + e.price * 0.2
-              }`}</p>
-              <p
-                className={`z-0 ${openSans.className} text-lg text-[#bfa75d] `}
-              >{`R$ ${e.price}`}</p>
+            <div className='w-full h-full flex flex-col items-center p-2'>
+              <Link
+                href={`/product/${e.id}`}
+                className={`font-semibold hover:font-bold text-lg max-md:text-base`}
+              >
+                {e.name}
+              </Link>
+              <div className='pb-4 flex flex-col items-center'>
+                <p className='text-gray-500 line-through'>{`R$ ${
+                  e.price + e.price * 0.2
+                }`}</p>
+                <p
+                  className={`z-0 ${openSans.className} text-lg text-[#bfa75d] `}
+                >{`R$ ${e.price}`}</p>
+              </div>
             </div>
           </div>
         ))}
